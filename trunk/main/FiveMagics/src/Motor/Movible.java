7be6f977680786fe996de839349ad5994b0c3ff2
kreig.iron <kreig.iron@069a1d1c-994f-0410-a565-491aa208fe05>
2008-06-27 04:33:49 +0000

    - Agregadas clases del motor

---
+++ trunk/main/FiveMagics/src/Motor/Movible.java
@@ -0,0 +1,15 @@
+package Motor;
+
+
+// <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+// #[regen=yes,id=DCE.ADB19E0A-C336-08FA-5675-79D10E141A11]
+// </editor-fold> 
+public interface Movible {
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.15AC503A-EBCA-2C27-32D3-3DCDC6FA7F79]
+    // </editor-fold> 
+    public void Mover (int direccion, int pasos);
+
+}
+
