159617d617a8445cf6e773bf614afef9769f4dbf
kreig.iron <kreig.iron@069a1d1c-994f-0410-a565-491aa208fe05>
2008-06-28 04:31:01 +0000

    Build 1.0

---
+++ main/FiveMagics/src/Motor/Habilidad.java
@@ -0,0 +1,48 @@
+package Motor;
+
+import gui.Casilla; 
+
+// <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+// #[regen=yes,id=DCE.CAE0DC72-2F3C-8233-7443-AAE075D86640]
+// </editor-fold> 
+public abstract class Habilidad extends Personaje {
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.FF563FEC-1F70-7675-D698-C0008364C2CD]
+    // </editor-fold> 
+    protected Personaje p;
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.C36D31F6-62A7-6E41-7793-77926E6A8907]
+    // </editor-fold> 
+    public Habilidad () {
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,regenBody=yes,id=DCE.0B09490D-99A4-055D-E95B-A49DAA1DB39D]
+    // </editor-fold> 
+    public Personaje getP () {
+        return p;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,regenBody=yes,id=DCE.A30AED98-2A8B-416B-F8F7-C07EB29E3CE1]
+    // </editor-fold> 
+    public void setP (Personaje val) {
+        this.p = val;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.BCC350FA-AA62-0835-076F-4F9969E8530E]
+    // </editor-fold> 
+    public void atacar (Casilla c) {
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.78667E26-CDFA-07B6-A345-D2F81122A9DC]
+    // </editor-fold> 
+    public void defender () {
+    }
+
+}
+
